9599a8a6839812f05e4547b76a25d75a331d0e5e
anouksha <anu_blue21@hotmail.com>
2014-09-16 00:02:33 -0400

    Debugging code

--- numbersfromaccounts.py
+++ numbersfromaccounts.py
@@ -6,17 +6,18 @@ from sets import Set
 db = pymongo.MongoClient().Twitter
 db_parsed = pymongo.MongoClient().TwitterParsed
 
-tweets = db.tweets.find()
+tweets = db.tweets.find().limit(200)
 accounts = []
 
 for tweet in tweets:
-    accounts.append(tweet['user']['screen_name'])
+    #accounts.append(tweet['user']['screen_name'])
+    print tweet['user']['screen_name']
 
-account_names = Set(accounts)
+'''account_names = Set(accounts)
 
 print account_names
 
-'''for name in account_names:
+for name in account_names:
     tweet_ids = db.tweets.find({"user.screen_name":name}).distinct("id")
     numbers = []
     phones = db_parsed.phones.find({"id":{"$in":tweet_ids}})
