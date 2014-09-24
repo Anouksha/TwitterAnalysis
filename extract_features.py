3a4ea2b4f26bc34a4ecc5d37a0e35597ae0f0157
anouksha <anu_blue21@hotmail.com>
2014-09-24 19:13:33 -0400

    Code for extracting features for each phone number

---
+++ extract_features.py
@@ -0,0 +1,26 @@
+__author__ = 'anouksha'
+
+import pymongo
+
+db_parsed = pymongo.MongoClient().TwitterParsed
+phone_numbers = db_parsed.phonestrain.distinct("phone_no").limit(10)
+
+
+def getPhoneCount(tweets):
+    return tweets.count()
+
+def getUniqueRepresentations(tweets):
+    original = tweets.distinct("o_pno")
+    return original
+
+print "Starting feature extraction"
+
+for phone in phone_numbers:
+    tweets = db_parsed.phonestrain.find({"phone_no":phone})
+    data = []
+    data['phone_no'] = phone
+    data['count'] = getPhoneCount(tweets)
+    data['representations'] = getUniqueRepresentations(tweets)
+
+
+
