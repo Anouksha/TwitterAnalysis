822476472f42b05091653513652528da5e64d032
anouksha <anu_blue21@hotmail.com>
2014-09-24 19:17:45 -0400

    Testing initial run

--- extract_features.py
+++ extract_features.py
@@ -17,11 +17,11 @@ print "Starting feature extraction"
 
 for phone in phone_numbers:
     tweets = db_parsed.phonestrain.find({"phone_no":phone})
-    data = []
+    data = {}
     data['phone_no'] = phone
     data['count'] = getPhoneCount(tweets)
     data['representations'] = getUniqueRepresentations(tweets)
     break
 
-
+print data
 
