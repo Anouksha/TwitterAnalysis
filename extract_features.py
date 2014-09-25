66ccc29f46a9023a7209aa4d1871fe124a842258
anouksha <anu_blue21@hotmail.com>
2014-09-25 18:35:14 -0400

    Edited code for standard deviation function

--- extract_features.py
+++ extract_features.py
@@ -64,7 +64,7 @@ for phone in phone_numbers:
     data['representations'] = get_unique_representations(tweets)
     data['num_accounts'] = get_num_of_accounts(tweets)
     data['mean_text'] = get_mean_text(tweets)
-    data['deviation_text'] = get_std_dev_text(tweets, data['mean_text'])
+    #data['deviation_text'] = get_std_dev_text(tweets, data['mean_text'])
     data['first_occurrence'] = get_first_occurrence(tweets)
     data['last_occurrence'] = get_last_occurrence(tweets)
     c+=1
