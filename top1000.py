4e3f885412a16f9c4a8bca1c49bbdd49cf2495c6
anouksha <anu_blue21@hotmail.com>
2014-10-01 10:38:42 -0400

    Using map reduce instead

--- top1000.py
+++ top1000.py
@@ -1,11 +1,37 @@
 __author__ = 'anouksha'
 import pymongo
+from bson.code import Code
 
 db = pymongo.MongoClient().TwitterParsed
 
-numbers = db.phonestrain.aggregate([{"$group":{"_id":"$phone_no", "count":{"$sum":1}}},{"$sort":{"count":-1}}]).limit(1000)
+'''numbers = db.phonestrain.aggregate([{"$group":{"_id":"$phone_no", "count":{"$sum":1}}},{"$sort":{"count":-1}}]).limit(1000)
 
 for number in numbers:
     print number
 
+print "Done"'''
+
+mapper = Code("""function () {
+                this.phone_no.forEach(function(z) {
+                  emit(z, 1);
+                });
+             }
+              """)
+reducer = Code("""
+              function (key, values) {
+                var total = 0;
+                  for (var i = 0; i < values.length; i++) {
+                    total += values[i];
+                  }
+                  return total;
+                }
+                """)
+
+result = db.phonestrain.map_reduce(mapper, reducer, "myresults")
+for doc in result.find():
+    print doc
+
 print "Done"
+
+
+
