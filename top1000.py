1bdfd1043ef658e26fecf18956439843234d548c
anouksha <anu_blue21@hotmail.com>
2014-10-01 10:11:40 -0400

    Added extra bracket

--- top1000.py
+++ top1000.py
@@ -3,7 +3,7 @@ import pymongo
 
 db = pymongo.MongoClient().TwitterParsed
 
-numbers = db.phonestrain.aggregate({"$group":{"_id":"$phone_no", "count":{"$sum":1}}},{"$sort":{"count":-1}}).limit(1000)
+numbers = db.phonestrain.aggregate([{"$group":{"_id":"$phone_no", "count":{"$sum":1}}},{"$sort":{"count":-1}}]).limit(1000)
 
 for number in numbers:
     print number
