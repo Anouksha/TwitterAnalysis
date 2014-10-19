14ca42ccafa7564c78646dce31185b38c786e5cd
anouksha <anu_blue21@hotmail.com>
2014-10-19 15:59:10 -0400

    Code to change date format

--- change_date_format.py
+++ change_date_format.py
@@ -9,8 +9,12 @@ tweets = db.bharat_phonestrain.find()
 print "Starting"
 
 for tweet in tweets:
-    tweet['created_at'] = datetime.datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
+    print tweet['created_at']
+    tweet['created_at'] = datetime.datetime.strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')
     print tweet
     db.bharat_phonestrain.save(tweet)
+    break
 
 print "Done"
+
+#db.collection.find().forEach(function (tweet){db.collection.update({_id: tweet._id}, {$set: {created_at: new Date(tweet.created_at)}});});
